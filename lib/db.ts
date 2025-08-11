6f3682379ded6007c9291819123d2c971376cb1e
ShubhamRoy123 <roy388925@gmail.com>
2025-08-11 19:46:21 +0530

    Connect project to MongoDB Atlas cluster

--- lib/db.ts
+++ lib/db.ts
@@ -1,41 +1,41 @@
+
 import mongoose from 'mongoose';
 
-
-const MONGOOSE_URI = process.env.MONGOOSE_URI!;
+const MONGOOSE_URI = 'mongodb+srv://roy388925:JE1Em1OgYF9RFmQ2@instagram-clone.jo3ml6n.mongodb.net/?retryWrites=true&w=majority';
 
 if (!MONGOOSE_URI) {
-  throw new Error('MONGOOSE_URI environment variable is not set');
+    throw new Error('MongoDB connection string is not set');
 }
 
-let cached = global.mongoose;
 
+
+interface MongooseCache {
+    conn: typeof mongoose.connection | null;
+    promise: Promise<typeof mongoose.connection> | null;
+}
+
+let cached: MongooseCache = (global as any).mongoose;
 if (!cached) {
-  cached = global.mongoose = { conn: "", promise: null }
+    cached = (global as any).mongoose = { conn: null, promise: null };
 }
+
 
 export async function connectToDatabase() {
     if (cached.conn) {
         return cached.conn;
     }
-
     if (!cached.promise) {
         const opts = {
             bufferCommands: true,
-            maxPoolSize: 10 
+            maxPoolSize: 10,
         };
-
-         cached.promise = mongoose
-    .connect(MONGOOSE_URI, opts)
-    .then(() => mongoose.connection);
+        cached.promise = mongoose.connect(MONGOOSE_URI, opts).then(() => mongoose.connection);
     }
-
-    try{
+    try {
         cached.conn = await cached.promise;
         return cached.conn;
-    }catch (error) {
+    } catch (error) {
         cached.promise = null;
         throw error;
     }
-
-    return cached.conn;
 }
